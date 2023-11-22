b993b00f2903eaf934d0bbbcc288f9cdfb8e3aa8
darko <ki14.skakun@gmail.com>
2023-11-22 19:11:10 +0200

    added challenges

---
+++ easy/count-ones-in-2d-array.js
@@ -0,0 +1,35 @@
+/*
+Count Ones in a 2D Array
+
+Create a function to count the number of 1s in a 2D array.
+
+Examples
+countOnes([
+  [1, 0],
+  [0, 0]
+]) ➞ 1
+
+countOnes([
+  [1, 1, 1],
+  [0, 0, 1],
+  [1, 1, 1]
+]) ➞ 7
+
+countOnes([
+  [1, 2, 3],
+  [0, 2, 1],
+  [5, 7, 33]
+]) ➞ 2
+
+
+*/
+
+function countOnes(matrix){
+    return matrix.flat(2).filter(n=>n===1).length;
+}
+
+console.log(countOnes([
+    [1, 1, 1],
+    [0, 0, 1],
+    [1, 1, 1]
+  ]));
---
+++ easy/return-highest-lowest-number.js
@@ -0,0 +1,32 @@
+/*
+Return the Highest and Lowest Numbers
+
+Create a function that accepts a string of space separated 
+numbers and returns the highest and lowest number (as a string).
+
+Examples
+highLow("1 2 3 4 5") ➞ "5 1"
+
+highLow("1 2 -3 4 5") ➞ "5 -3"
+
+highLow("1 9 3 4 -5") ➞ "9 -5"
+
+highLow("13") ➞ "13 13"
+
+Notes
+
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.
+
+
+*/
+
+function highLow(str){
+    str = str.split(" ").sort((a,b)=> a-b);
+    return str.length>1?`${str[str.length-1]} ${str[0]}`: `${str[0]} ${str[0]}`; 
+}
+
+console.log(highLow("1 2 3 4 5"));
+console.log(highLow("1 2 -3 4 5"));
+console.log(highLow("13"));
