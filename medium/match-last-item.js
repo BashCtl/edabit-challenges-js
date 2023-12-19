5543186affa3ddf7e3eba19a2b3b54ca2bcc0319
darko <ki14.skakun@gmail.com>
2023-12-19 17:51:11 +0200

    added challenges

---
+++ medium/match-last-item.js
@@ -0,0 +1,25 @@
+/*
+Match the Last Item
+
+Create a function that takes an array of items and checks if the last 
+item matches the rest of the array concatenated together.
+
+Examples
+matchLastItem(["rsq", "6hi", "g", "rsq6hig"]) ➞ true
+// The last item is the rest joined.
+
+matchLastItem([1, 1, 1, "11"]) ➞ false
+// The last item should be "111".
+
+matchLastItem([8, "thunder", true, "8thundertrue"]) ➞ true
+
+Notes
+The array is always filled with items.
+
+*/
+
+function matchLastItem(arr) {
+  return arr.slice(0, arr.length - 1).join("") === arr[arr.length - 1];
+}
+
+console.log(matchLastItem(["rsq", "6hi", "g", "rsq6hig"]));
