60c5767e51bcb80256e28d75cd0900cbf715712b
darko <ki14.skakun@gmail.com>
2023-12-23 18:37:45 +0200

    added challenges

---
+++ hard/combinations..js
@@ -0,0 +1,26 @@
+/*
+Combinations
+
+
+Create a function that takes a variable number of arguments, 
+each argument representing the number of items in a group. 
+The function should return the number of permutations (combinations) 
+of choices you would have if you selected one item from each group.
+
+Examples
+combinations(2, 3) ➞ 6
+
+combinations(3, 7, 4) ➞ 84
+
+combinations(2, 3, 4, 5) ➞ 120
+
+Notes
+Don't overthink this one.
+Input may include the number zero.
+*/
+
+function combinations(...items) {
+  return items.filter((a) => a !== 0).reduce((a, c) => a * c, 1);
+}
+
+console.log(combinations(3, 7, 4));
