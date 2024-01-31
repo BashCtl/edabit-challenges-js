2f7f643d62d425290f74112f4ed7a3c6b523e24b
darko <ki14.skakun@gmail.com>
2024-01-31 08:42:40 +0200

    who left the array

---
+++ hard/who-left-array.js
@@ -0,0 +1,39 @@
+/*
+Who Left the Array?
+
+You are given two arrays. The elements in arr1 threw a party and started to mix around. 
+However, one of the elements got lost! Your task is to return the element which was lost.
+
+Examples
+missing([1, 2, 3, 4, 5, 6, 7, 8], [1, 3, 4, 5, 6, 7, 8]) ➞ 2
+
+missing([true, true, false, false, true], [false, true, false, true]) ➞ true
+
+missing(["Jane", "is", "pretty", "ugly"], ["Jane", "is", "pretty"]) ➞ "ugly"
+
+Notes
+Assume that the first array always contains 1 or more elements.
+Elements are always lost.
+An element can also have duplicates.
+
+*/
+
+function missing(arr1, arr2) {
+  arr1 = arr1.sort();
+  arr2 = arr2.sort();
+  let length = arr2.length;
+
+  for (let i = 0; i < length; i++) {
+    if (arr1[i] != arr2[i]) {
+      return arr1[i];
+    }
+  }
+
+  return arr1[arr1.length - 1];
+}
+
+console.log(missing([1, 2, 3, 4, 5, 6, 7, 8], [1, 3, 4, 5, 6, 7, 8]));
+console.log(
+  missing([true, true, false, false, true], [false, true, false, true])
+);
+console.log(missing(["Jane", "is", "pretty", "ugly"], ["Jane", "is", "pretty"]))
